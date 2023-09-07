dfc91988c1d23c44b559bb4adf493a32f627af88
AnnaSutasinee <annalena830@gmail.com>
2023-09-07 09:17:40 +0200

    Fix Code delete static url, grant permission using tabs

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-// 1. Grab the SAVE TAB button and store it in a tabBtn variable
 const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
@@ -12,21 +11,24 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    {url: "www.linkedin.com/in/annalena-roos-047277269"}
-]
 
 // 2. Stack Overfolow How to get current active tab
 tabBtn.addEventListener("click", function(){
-    chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
-    let activeTabs = tabs[0]
-    let activeTabId = activeTab.id; 
-    });
-    
+  //chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
+   // let activeTabs = tabs[0]
+   //let activeTabId = activeTab.id; 
+    //});
+    //Grands access of the current active tabs in Chrome, pick only my current tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //function will be triggered when Chrome found the tab we told Chrome to look for
+    //gives us back a tabs variable
     myLeads.push(tabs[0].url)
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
-});
+    }) 
+    
+   
+})
 
 function render(leads) {
     let listItems = ""
